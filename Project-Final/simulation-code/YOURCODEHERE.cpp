c2f15bddc7b2901a1d834f511730befb584a3639
rishabhbhatt009 <rishabhbhatt009@gmail.com>
2023-05-04 23:30:14 -0400

    completed basic requirements

--- Project-Final/simulation-code/YOURCODEHERE.cpp
+++ Project-Final/simulation-code/YOURCODEHERE.cpp
@@ -14,6 +14,43 @@
 #include "431project.h"
 
 
+// util functions
+unsigned int getdl1size(std::string configuration);
+unsigned int getil1size(std::string configuration);
+unsigned int getl2size(std::string configuration);
+
+unsigned int getl1lat(int size, int assoc){  
+  int latency = log2(size) - 12;
+
+  if(assoc==1){
+    return latency;
+  }
+  if(assoc==2){
+    return latency+1;
+  }
+  if(assoc==4){
+    return latency+2;
+  }
+}
+unsigned int getl2lat(int size, int assoc){  
+  int latency = log2(size) - 10;
+  
+  if(assoc==1){
+    return latency-2;
+  }
+  if(assoc==2){
+    return latency-1;
+  }
+  if(assoc==4){
+    return latency;
+  }
+  if(assoc==8){
+    return latency+1;
+  }
+  if(assoc==16){
+    return latency+2;
+  }
+}
 
 
 /*
@@ -40,60 +77,71 @@ int validateConfiguration(std::string configuration){
   }
 
   // declatring vars 
-  unsigned int ifq = 8 * (1 << configurationDimsAsInts[0]);
+  unsigned int ifq = (1 << configurationDimsAsInts[0]);
   unsigned int il1_block_size = 8 * (1 << configurationDimsAsInts[0]);
   unsigned int dl1_block_size = 8 * (1 << configurationDimsAsInts[0]);
 
-  unsigned int ruu_size = 4 << configurationDimsAsInts[3]
-  unsigned int lsq_size = 4 << configurationDimsAsInts[4]
+  unsigned int ruu_size = 4 << configurationDimsAsInts[3];
+  unsigned int lsq_size = 4 << configurationDimsAsInts[4];
 
-  unsigned int dl1_sets = 32 << configurationDimsAsInts[6]
-  unsigned int dl1_assoc = 1 << configurationDimsAsInts[7]
-  unsigned int il1_sets = 32 << configurationDimsAsInts[8]
-  unsigned int il1_assoc = 1 << configurationDimsAsInts[9]
+  unsigned int dl1_sets = 32 << configurationDimsAsInts[6];
+  unsigned int dl1_assoc = 1 << configurationDimsAsInts[7];
+  unsigned int il1_sets = 32 << configurationDimsAsInts[8];
+  unsigned int il1_assoc = 1 << configurationDimsAsInts[9];
 
-  
-  unsigned int ul2_block_size =  16 << (configurationDimsAsInts[11]);
-  unsigned int ul2_assoc = 1 << (configurationDimsAsInts[12])
+
+  unsigned int l2_block_size =  16 << (configurationDimsAsInts[11]);
+  unsigned int l2_assoc = 1 << (configurationDimsAsInts[12]);
+
+  unsigned int dl1_lat = configurationDimsAsInts[14];
+  unsigned int il1_lat = configurationDimsAsInts[15];
+  unsigned int l2_lat = configurationDimsAsInts[16];
+
 
   unsigned int il1_size = getil1size(configuration);
   unsigned int dl1_size = getdl1size(configuration);
-  unsigned int ul2_size = getl2size(configuration);  
+  unsigned int l2_size = getl2size(configuration);  
 
-    
-  // Check 2 : il1 block size must match the ifq size 
-  if (il1_block_size < ifq){
-    return 0;
-  }
+  // Check 2 : il1 block size must match the ifq size (already enforced)
+  // if (il1_block_size < ifq){
+  //   return 0;
+  // }
   
   // Check 3 : dl1 should have the same block size as your il1
   if (dl1_block_size != il1_block_size){
     return 0; 
   }
   
-  // Check 4 : 
-
+  // Check 4 : latency mapping for L1 (inst, data)
+  if (dl1_lat != getl1lat(dl1_size, dl1_assoc) || il1_lat != getl1lat(il1_size, il1_assoc)){
+    return 0 ; 
+  }
 
   // Check 5 : ul2 block size must be at least twice your il1 (dl1) block size 
-  if (ul2_block_size < 2*il1_block_size || ul2_block_size < 2*dl1_block_size){
+  if (l2_block_size < 2*il1_block_size || l2_block_size < 2*dl1_block_size){
     return 0;
   }
   
   // Check 6 : ul2 block size : maximum block size of 128B. 
-  if (ul2_block_size > 128){
+  if (l2_block_size > 128){
     return 0;
   }
   
   // Check 7 : ul2 must be at least as large as il1+dl1 in order to be inclusive.
-  if (ul2_size < il1_size+dl1_size){
+  if (l2_size < il1_size+dl1_size){
     return 0;
   }
-  
-  // Check 8
-  
+
+  // Check 8 : latency mapping for L2
+  if (l2_lat != getl2lat(l2_size, l2_assoc)){
+    return 0;
+  }  
 }
 
 
+/*
+ * Generate random numbers within range (inclusive)
+*/
 int getRandomNumber(int lower, int upper) {
     // Generate a random integer between the lower and upper bounds
     int random_num = std::rand() % (upper - lower + 1) + lower;
@@ -110,31 +158,50 @@ std::string YourProposalFunction(std::string currentconfiguration, std::string b
   std::string nextconfiguration=GLOB_baseline;
   
   int configurationDimsAsInts[18];
-  extractConfiguration(configuration, configurationDimsAsInts); // Configuration parameters now available in array
+  extractConfiguration(currentconfiguration, configurationDimsAsInts); // Configuration parameters now available in array
 
 
   if (optimizeforEXEC == 1){
     
     // update configurationDimsAsInts
     
-    configurationDimsAsInts[0] = getRandomNumber(0,3)
-    configurationDimsAsInts[1] = 0 // default 
-    configurationDimsAsInts[2] = getRandomNumber(0,1)
-    configurationDimsAsInts[3] = 0 // default 
-    configurationDimsAsInts[4] = 0 // default 
-    configurationDimsAsInts[5] = 0 // default 
-    configurationDimsAsInts[6] = 5 // default
-    configurationDimsAsInts[7] = 0 // default 
-    configurationDimsAsInts[8] = 5 // default 
-    configurationDimsAsInts[9] = 0 // default 
-    configurationDimsAsInts[10] = 2 // default 
-    configurationDimsAsInts[11] = 2 // default 
-    configurationDimsAsInts[12] = 2 // default 
-    configurationDimsAsInts[13] = 3 // default 
-    configurationDimsAsInts[14] = 0 // default 
-    configurationDimsAsInts[15] = 0 // default 
-    configurationDimsAsInts[16] = 3 // default 
-    configurationDimsAsInts[17] = getRandomNumber(0,5)
+    configurationDimsAsInts[0] = getRandomNumber(0,3);
+    configurationDimsAsInts[1] = 0; // default 
+    configurationDimsAsInts[2] = getRandomNumber(0,1);
+    configurationDimsAsInts[3] = 0; // default 
+    configurationDimsAsInts[4] = 0; // default 
+    configurationDimsAsInts[5] = 0; // default 
+    configurationDimsAsInts[6] = 5; // default
+    configurationDimsAsInts[7] = 0; // default 
+    configurationDimsAsInts[8] = getRandomNumber(0,8); // il1 
+    configurationDimsAsInts[9] = getRandomNumber(0,2); // il1 
+    configurationDimsAsInts[10] = 2; // default 
+    configurationDimsAsInts[11] = 2; // default 
+    configurationDimsAsInts[12] = 2; // default 
+    configurationDimsAsInts[13] = 3; // TLB default
+
+    // mapping latency 
+    unsigned int dl1_sets = 32 << configurationDimsAsInts[6];
+    unsigned int dl1_assoc = 1 << configurationDimsAsInts[7];
+    unsigned int dl1_block_size = 8 * (1 << configurationDimsAsInts[0]);
+    unsigned int il1_sets = 32 << configurationDimsAsInts[8];
+    unsigned int il1_assoc = 1 << configurationDimsAsInts[9];
+    unsigned int il1_block_size = 8 * (1 << configurationDimsAsInts[0]);
+  
+    unsigned int l2_sets = 256 << (configurationDimsAsInts[10]);
+    unsigned int l2_block_size =  16 << (configurationDimsAsInts[11]);
+    unsigned int l2_assoc = 1 << (configurationDimsAsInts[12]); 
+
+    unsigned int il1_size = il1_assoc*il1_sets*il1_block_size;
+    unsigned int dl1_size = dl1_assoc*dl1_sets*dl1_block_size;
+    unsigned int l2_size = l2_assoc*l2_sets*l2_block_size;  
+
+    configurationDimsAsInts[14] = getl1lat(il1_size, il1_assoc); 
+    configurationDimsAsInts[15] = getl1lat(dl1_size, dl1_assoc); 
+    configurationDimsAsInts[16] = getl2lat(l2_size, l2_assoc); 
+
+
+    configurationDimsAsInts[17] = getRandomNumber(0,5);
 
   }
 
@@ -142,25 +209,43 @@ std::string YourProposalFunction(std::string currentconfiguration, std::string b
 
     // update configurationDimsAsInts
     
-    configurationDimsAsInts[0] = getRandomNumber(0,3)
-    configurationDimsAsInts[1] = 0 // default 
-    configurationDimsAsInts[2] = getRandomNumber(0,1)
-    configurationDimsAsInts[3] = 0 // default 
-    configurationDimsAsInts[4] = 0 // default 
-    configurationDimsAsInts[5] = 0 // default 
-    configurationDimsAsInts[6] = 5 // default
-    configurationDimsAsInts[7] = 0 // default 
-    configurationDimsAsInts[8] = 5 // default 
-    configurationDimsAsInts[9] = 0 // default 
-    configurationDimsAsInts[10] = 2 // default 
-    configurationDimsAsInts[11] = 2 // default 
-    configurationDimsAsInts[12] = 2 // default 
-    configurationDimsAsInts[13] = 3 // default 
-    configurationDimsAsInts[14] = 0 // default 
-    configurationDimsAsInts[15] = 0 // default 
-    configurationDimsAsInts[16] = 3 // default 
-    configurationDimsAsInts[17] = getRandomNumber(0,5)
+    configurationDimsAsInts[0] = getRandomNumber(0,3);
+    configurationDimsAsInts[1] = 0; // default 
+    configurationDimsAsInts[2] = getRandomNumber(0,1);
+    configurationDimsAsInts[3] = 0; // default 
+    configurationDimsAsInts[4] = 0; // default 
+    configurationDimsAsInts[5] = 0; // default 
+    configurationDimsAsInts[6] = getRandomNumber(0,8); // dl1
+    configurationDimsAsInts[7] = getRandomNumber(0,2); // dl1 
+    configurationDimsAsInts[8] = 5; // default 
+    configurationDimsAsInts[9] = 0; // default 
+    configurationDimsAsInts[10] = getRandomNumber(0,9); // default 
+    configurationDimsAsInts[11] = getRandomNumber(0,3); // default 
+    configurationDimsAsInts[12] = getRandomNumber(0,4); // default 
+    configurationDimsAsInts[13] = 3; // TLB default 
 
+    // mapping latency 
+    unsigned int dl1_sets = 32 << configurationDimsAsInts[6];
+    unsigned int dl1_assoc = 1 << configurationDimsAsInts[7];
+    unsigned int dl1_block_size = 8 * (1 << configurationDimsAsInts[0]);
+    unsigned int il1_sets = 32 << configurationDimsAsInts[8];
+    unsigned int il1_assoc = 1 << configurationDimsAsInts[9];
+    unsigned int il1_block_size = 8 * (1 << configurationDimsAsInts[0]);
+  
+    unsigned int l2_sets = 256 << (configurationDimsAsInts[10]);
+    unsigned int l2_block_size =  16 << (configurationDimsAsInts[11]);
+    unsigned int l2_assoc = 1 << (configurationDimsAsInts[12]); 
+
+    unsigned int il1_size = il1_assoc*il1_sets*il1_block_size;
+    unsigned int dl1_size = dl1_assoc*dl1_sets*dl1_block_size;
+    unsigned int l2_size = l2_assoc*l2_sets*l2_block_size;  
+
+    configurationDimsAsInts[14] = getl1lat(il1_size, il1_assoc); 
+    configurationDimsAsInts[15] = getl1lat(dl1_size, dl1_assoc); 
+    configurationDimsAsInts[16] = getl2lat(l2_size, l2_assoc); 
+
+    
+    configurationDimsAsInts[17] = getRandomNumber(0,5);
   }   
 
   else {
@@ -176,7 +261,16 @@ std::string YourProposalFunction(std::string currentconfiguration, std::string b
   nextconfiguration=ss.str();
   ss.str("");    
 
-  return nextconfiguration;
+
+  if (validateConfiguration(nextconfiguration)){
+    std::cout << "Valid Configuration     : " << nextconfiguration << std::endl;
+    return nextconfiguration;
+  }
+  else{
+    std::cout << "Invalid Configuration     : " << nextconfiguration << std::endl;
+    nextconfiguration = YourProposalFunction(currentconfiguration, bestEXECconfiguration, bestEDPconfiguration, optimizeforEXEC, optimizeforEDP);
+  }
+  
 
 }
 
